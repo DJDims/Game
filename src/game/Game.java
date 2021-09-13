03e1bdc15de817bf8e634f8ed9da466031e64bde
pupil <pupil@ivkhk.ee>
2021-09-13 12:38:01 +0300

    Desjatpopitok

--- src/game/Game.java
+++ src/game/Game.java
@@ -9,16 +9,26 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         
+        boolean win = false;
+
         int number = random.nextInt(10);
 
-        System.out.println("Программа загадала число от 0 до 9. Отгадайте число.");
-        // System.out.println(number);
+        System.out.println("Программа загадала число от 0 до 9. У вас 10 попыток. Отгадайте число.");
+        // System.out.println(number);  //Сразу вывести загаданное число
 
-        int userNumber = scanner.nextInt();
-        
-        if(userNumber == number){
-            System.out.println("Поздравляю. Вы победили!");
-        }else{
+        for (int i = 0; i < 10; i++) {     
+            int userNumber = scanner.nextInt();
+            
+            if(userNumber == number){
+                System.out.println("Поздравляю. Вы победили!");
+                System.out.println("Вы отгадали число с " + ++i + " попытки");
+                win = true;
+                break;
+            }else{
+                System.out.println("Неверно. Попробуйте еще раз.");
+            }
+        }
+        if (!win) {
             System.out.println("Вы проиграли. Загаданное число = " + number);
         }
     }
